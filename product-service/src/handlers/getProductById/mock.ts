67f1d7d425e6250c0bbf933ad2af0f57a64dfab9
Gleb Kazak <zilian159@gmail.com>
2023-03-22 09:25:51 +0300

    Task3: product lambdas

---
+++ product-service/src/handlers/getProductById/mock.ts
@@ -0,0 +1,25 @@
+import {Product} from "../product";
+
+export const data: Product[] = [
+  {
+    "id": 1,
+    "title": "Fishing Rod",
+    "description": "Uniq fishing rod for extremal condition",
+    "count": 12,
+    "price": 100
+  },
+  {
+    "id": 2,
+    "title": "Plotva bait",
+    "description": "Plotva loves it",
+    "count": 50,
+    "price": 6
+  },
+  {
+    "id": 3,
+    "title": "Fishing chair",
+    "description": "Comfortable chair",
+    "count": 20,
+    "price": 400
+  }
+]
