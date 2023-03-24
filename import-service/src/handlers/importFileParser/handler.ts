406022c8374829490798497e0a5a7dee807ba6d9
Gleb Kazak <zilian159@gmail.com>
2023-03-25 01:21:28 +0300

    Task5: move to 3v SDK

--- import-service/src/handlers/importFileParser/handler.ts
+++ import-service/src/handlers/importFileParser/handler.ts
@@ -1,9 +1,10 @@
 import {Callback, Context, S3Event} from "aws-lambda";
-import {S3} from "aws-sdk";
+import { S3Client, GetObjectCommand, CopyObjectCommand, DeleteObjectCommand } from "@aws-sdk/client-s3";
 import csv from 'csv-parser';
+import type {Readable} from 'stream'
 
 export const importFileParser = async (event: S3Event, context: Context, callback: Callback): Promise<void> => {
-    const s3 = new S3({signatureVersion: 'v4', region: "us-east-1"});
+    const client = new S3Client({region: "us-east-1"});
     const bucket = event.Records[0].s3.bucket.name;
     const key = decodeURIComponent(event.Records[0].s3.object.key.replace(/\+/g, ' '));
     const params = {
@@ -11,28 +12,32 @@ export const importFileParser = async (event: S3Event, context: Context, callbac
         Key: key,
     };
     try {
-        const stream = await s3.getObject(params).createReadStream();
-        const promise = new Promise((resolve, reject) => {
-            stream
-                .pipe(csv())
-                .on('data', async (data) => {
-                    console.log(data);
-                })
-                .on("close", () => {
-                    callback();
-                    resolve(1);
-                })
-                .on("error", () => {
-                    callback();
-                    reject();
-                })
-                .on('end', async () => {
-                    await moveFile(s3, bucket, key);
-                    callback();
-                    resolve(1);
-                });
-        });
-        await promise;
+        const result = await client.send(new GetObjectCommand(params));
+        if (result.Body) {
+            const stream = result.Body as Readable
+            const promise = new Promise((resolve, reject) => {
+                stream
+                    .pipe(csv())
+                    .on('data', async (data) => {
+                        console.log(data);
+                    })
+                    .on("close", () => {
+                        callback();
+                        resolve(1);
+                    })
+                    .on("error", () => {
+                        callback();
+                        reject();
+                    })
+                    .on('end', async () => {
+                        await moveFile(client, bucket, key);
+                        callback();
+                        resolve(1);
+                    });
+            });
+            await promise;
+        }
+
     } catch (err) {
         console.log(err)
         console.error(`Error getting object ${key} from bucket ${bucket}.`);
@@ -40,18 +45,20 @@ export const importFileParser = async (event: S3Event, context: Context, callbac
     }
 }
 
-async function moveFile(s3: S3, bucket: string, file: string) {
+async function moveFile(s3: S3Client, bucket: string, file: string) {
     const [_, fileName] = file.split('/');
     const sourceFolder = 'uploaded'
     const destFolder = 'parsed'
-    const s3Params = {
-        Bucket: bucket,
-        CopySource: `${bucket}/${sourceFolder}/${fileName}`,
-        Key: `${destFolder}/${fileName}`
-    };
     try {
-        await s3.copyObject(s3Params).promise();
-        await s3.deleteObject({Bucket: bucket, Key: `${sourceFolder}/${fileName}`}).promise();
+        await s3.send(new CopyObjectCommand({
+            Bucket: bucket,
+            CopySource: `${bucket}/${sourceFolder}/${fileName}`,
+            Key: `${destFolder}/${fileName}`
+        }));
+        await s3.send(new DeleteObjectCommand({
+            Bucket: bucket,
+            Key: `${sourceFolder}/${fileName}`
+        }));
     } catch (ex) {
         console.error(`Failed: ${ex}`)
     }
--- import-service/src/handlers/importProductsFile/handler.ts
+++ import-service/src/handlers/importProductsFile/handler.ts
@@ -1,5 +1,6 @@
 import {APIGatewayProxyEvent, APIGatewayProxyResult} from "aws-lambda";
-import {S3} from "aws-sdk";
+import {S3Client, PutObjectCommand} from "@aws-sdk/client-s3";
+import {getSignedUrl} from './my-s3-client-internals';
 
 export const importProductsFile = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
     const {REGION: region, BUCKET: bucket} = process.env;
@@ -8,8 +9,8 @@ export const importProductsFile = async (event: APIGatewayProxyEvent): Promise<A
     }
 
     const file = event.queryStringParameters
-            ? event.queryStringParameters['name']
-            : undefined;
+        ? event.queryStringParameters['name']
+        : undefined;
 
     if (!file) {
         return {
@@ -23,33 +24,20 @@ export const importProductsFile = async (event: APIGatewayProxyEvent): Promise<A
             }),
         };
     }
-
-    const params = {
+    const client = new S3Client({region: region});
+    const command = new PutObjectCommand({
         Bucket: bucket,
         Key: "uploaded/" + file,
-        Expires: 30,
+    });
+    const url = await getSignedUrl(client, command, {expiresIn: 3600});
+
+    return {
+        statusCode: 200,
+        headers: {
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Credentials': true,
+        },
+        body: JSON.stringify(url),
     };
 
-    try {
-        const s3 = new S3({signatureVersion: 'v4', region});
-        const url = s3.getSignedUrl('putObject', params);
-
-        return {
-            statusCode: 200,
-            headers: {
-                'Access-Control-Allow-Origin': '*',
-                'Access-Control-Allow-Credentials': true,
-            },
-            body: JSON.stringify(url),
-        };
-    } catch (error) {
-        return {
-            statusCode: 400,
-            headers: {
-                'Access-Control-Allow-Origin': '*',
-                'Access-Control-Allow-Credentials': true,
-            },
-            body: JSON.stringify(error),
-        };
-    }
 }
