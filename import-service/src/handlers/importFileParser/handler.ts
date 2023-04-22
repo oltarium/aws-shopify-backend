292d9fef6ea648423e3eecb895f942a9b5ee57f5
Gleb Kazak <gkazak@estee.com>
2023-04-23 01:35:25 +0300

    Task-6: sns

--- import-service/src/handlers/importFileParser/handler.ts
+++ import-service/src/handlers/importFileParser/handler.ts
@@ -1,9 +1,12 @@
 import {Callback, Context, S3Event} from "aws-lambda";
 import { S3Client, GetObjectCommand, CopyObjectCommand, DeleteObjectCommand } from "@aws-sdk/client-s3";
+import { SQSClient, SendMessageCommand } from "@aws-sdk/client-sqs";
 import csv from 'csv-parser';
 import type {Readable} from 'stream'
+const sqsClient = new SQSClient({ region: "us-east-1" });
 
 export const importFileParser = async (event: S3Event, context: Context, callback: Callback): Promise<void> => {
+    const accountId = JSON.stringify(context.invokedFunctionArn).split(':')[4];
     const client = new S3Client({region: process.env.REGION});
     const bucket = event.Records[0].s3.bucket.name;
     const key = decodeURIComponent(event.Records[0].s3.object.key.replace(/\+/g, ' '));
@@ -20,6 +23,7 @@ export const importFileParser = async (event: S3Event, context: Context, callbac
                     .pipe(csv())
                     .on('data', async (data) => {
                         console.log(data);
+                        await sendSQSEvent(data, accountId);
                     })
                     .on("close", () => {
                         callback();
@@ -42,6 +46,16 @@ export const importFileParser = async (event: S3Event, context: Context, callbac
         console.error(`Error getting object ${key} from bucket ${bucket}.`);
         throw new Error(`Error getting object ${key} from bucket ${bucket}.`);
     }
+}
+
+
+async function sendSQSEvent(product: any, accountId: string) {
+    const params = {
+        MessageGroupId: 'products',
+        MessageBody: JSON.stringify(product),
+        QueueUrl: `https://sqs.us-east-1.amazonaws.com/492825409148/catalogItemsQueue.fifo`,
+    };
+    await sqsClient.send(new SendMessageCommand(params));
 }
 
 async function moveFile(s3: S3Client, bucket: string, file: string) {
